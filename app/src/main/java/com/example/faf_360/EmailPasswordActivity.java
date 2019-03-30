35dd9ae8c82754a97718126b2072c70d97f0a7f2
John Caicedo <jc@johncaicedo.com.co>
2019-03-29 19:08:50 -0500

    Adición de todos los componentes de Login.

---
+++ app/src/main/java/com/example/faf_360/EmailPasswordActivity.java
@@ -0,0 +1,261 @@
+package com.example.faf_360;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.faf_360.utils.spUsuarios;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailPasswordActivity extends BaseActivity implements
+        View.OnClickListener {
+
+    private static final String TAG = "EmailPassword";
+
+    private TextView mStatusTextView;
+    private TextView mDetailTextView;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    private FirebaseAuth mAuth;
+
+    spUsuarios spUsuarios = new spUsuarios(this);
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_password);
+
+        // Views
+        mStatusTextView = findViewById(R.id.status);
+        mDetailTextView = findViewById(R.id.detail);
+        mEmailField = findViewById(R.id.fieldEmail);
+        mPasswordField = findViewById(R.id.fieldPassword);
+
+        // Buttons
+        findViewById(R.id.emailSignInButton).setOnClickListener(this);
+        findViewById(R.id.emailCreateAccountButton).setOnClickListener(this);
+        findViewById(R.id.signOutButton).setOnClickListener(this);
+        findViewById(R.id.verifyEmailButton).setOnClickListener(this);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.emailCreateAccountButton) {
+            createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        } else if (i == R.id.emailSignInButton) {
+            signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        } else if (i == R.id.signOutButton) {
+            signOut();
+        } else if (i == R.id.verifyEmailButton) {
+            sendEmailVerification();
+        }
+    }
+
+    private void createAccount(String email, String password) {
+//        Intent intent = new Intent(getApplicationContext(), RegisterAgeActivity.class);
+//        startActivity(intent);
+//        overridePendingTransition(R.anim.fromrightin, R.anim.fromleftout);
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            if (user.isEmailVerified()) {
+                                spUsuarios.saveId(user.getUid());
+                                finish();
+                            }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+
+                            Toast.makeText(EmailPasswordActivity.this,
+                                    task.getException().getLocalizedMessage(),
+                                    Toast.LENGTH_LONG).show();
+
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            String strMessage = task.getException().getLocalizedMessage();
+                            mStatusTextView.setText(strMessage);
+                        }
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        spUsuarios.saveId("");
+        updateUI(null);
+    }
+
+    private void sendEmailVerification() {
+        // Disable button
+        findViewById(R.id.verifyEmailButton).setEnabled(false);
+
+        // Send verification email
+        final FirebaseUser user = mAuth.getCurrentUser();
+
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Re-enable button
+                        findViewById(R.id.verifyEmailButton).setEnabled(true);
+
+                        if (task.isSuccessful()) {
+                            //Message
+                            Toast.makeText(EmailPasswordActivity.this,
+                                    getString(R.string.send_verification_email) + user.getEmail(),
+                                    Toast.LENGTH_LONG).show();
+
+                            //Sign out
+                            mAuth.signOut();
+
+                            //Reset Id
+                            spUsuarios.saveId("");
+
+                            //Quit
+                            finish();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this,
+                                    "Failed to send verification email.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError(getString(R.string.validate_required));
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError(getString(R.string.validate_required));
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+        if (password.length() <= 5){
+            mPasswordField.setError(getString(R.string.validate_password_error));
+            valid = false;
+        }
+
+        if (!isEmailValid(email)){
+            mEmailField.setError(getString(R.string.validate_email_error));
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    private boolean isEmailValid(String email) {
+        String regExpn =
+                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                        +"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                        +"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                        +"([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+
+        CharSequence inputStr = email;
+
+        Pattern pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+
+        if(matcher.matches())
+            return true;
+        else
+            return false;
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressDialog();
+        if (user != null) {
+            mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
+                    user.getEmail(), (user.isEmailVerified()) ? "Si" : "No"));
+            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+            findViewById(R.id.emailPasswordButtons).setVisibility(View.GONE);
+            findViewById(R.id.emailPasswordFields).setVisibility(View.GONE);
+            findViewById(R.id.signedInButtons).setVisibility(View.VISIBLE);
+
+            findViewById(R.id.verifyEmailButton).setEnabled(!user.isEmailVerified());
+
+            if (!user.isEmailVerified()) {
+                Toast.makeText(this,
+                        "Debe verificar su correo electrónico para continuar.",
+                        Toast.LENGTH_LONG).show();
+
+                findViewById(R.id.signOutButton).setEnabled(false);
+            }
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+            mDetailTextView.setText(null);
+
+            findViewById(R.id.emailPasswordButtons).setVisibility(View.VISIBLE);
+            findViewById(R.id.emailPasswordFields).setVisibility(View.VISIBLE);
+            findViewById(R.id.signedInButtons).setVisibility(View.GONE);
+        }
+    }
+
+    public void Regresar(View view) {
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/faf_360/utils/spUsuarios.java
@@ -0,0 +1,93 @@
+package com.example.faf_360.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.faf_360.models.Usuarios;
+
+public class spUsuarios {
+    private Context mContext;
+
+    final String REGISTER = "register";
+    final String DEFVALUE = "";
+    final String ID = "id";
+    final String AGE = "age";
+    final String FIRSTNAME = "firstname";
+    final String LASTNAME = "lastname";
+    final String EMAIL = "email";
+
+    public spUsuarios(Context pContext) {
+        mContext = pContext;
+    }
+
+    private SharedPreferences getSP() {
+        return mContext.getSharedPreferences(REGISTER, Context.MODE_PRIVATE);
+    }
+
+    private boolean saveSP (String key, String value){
+        boolean bRetorno = true;
+
+        try {
+            SharedPreferences.Editor spEditor = getSP().edit();
+            spEditor.putString(key, value);
+            spEditor.commit();
+        }
+        catch (Exception e) {
+            bRetorno = false;
+        }
+
+        return bRetorno;
+    }
+
+    public String getId() {
+        return getSP().getString(ID, DEFVALUE);
+    }
+
+    public boolean saveId(String id) {
+        return saveSP(ID, id);
+    }
+
+    public String getAge() {
+        return getSP().getString(AGE, DEFVALUE);
+    }
+
+    public boolean saveAge(String age) {
+        return saveSP(AGE, age);
+    }
+
+    public String getFirstName() {
+        return getSP().getString(FIRSTNAME, DEFVALUE);
+    }
+
+    public boolean saveFirstName(String firstname) {
+        return saveSP(FIRSTNAME, firstname);
+    }
+
+    public String getLastName() {
+        return getSP().getString(LASTNAME, DEFVALUE);
+    }
+
+    public boolean saveLastName(String lastname) {
+        return saveSP(LASTNAME, lastname);
+    }
+
+    public String getEmail() {
+        return getSP().getString(EMAIL, DEFVALUE);
+    }
+
+    public boolean saveEmail(String email) {
+        return saveSP(EMAIL, email);
+    }
+
+    public Usuarios getUsuarios() {
+        Usuarios s = new Usuarios();
+
+        s.setId(getId());
+        s.setFirstname(getFirstName());
+        s.setLastname(getLastName());
+        s.setEmail(getEmail());
+        s.setIsConnected(false);
+
+        return s;
+    }
+}
